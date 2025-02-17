bade7f8739784be56840f74827296cbaa27b2d30
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-02-17 19:38:03 +0100

    Add Classroom/ClassroomDto mapper

---
+++ src/main/java/dev/jcasaslopez/classroom/mapper/ClassroomMapper.java
@@ -0,0 +1,27 @@
+package dev.jcasaslopez.classroom.mapper;
+
+import org.springframework.stereotype.Component;
+
+import dev.jcasaslopez.classroom.dto.ClassroomDto;
+import dev.jcasaslopez.classroom.entity.Classroom;
+
+@Component
+public class ClassroomMapper {
+	
+	Classroom classroomDtoToClassroom (ClassroomDto classroom) {
+		return new Classroom(classroom.getIdClassroom(),
+							classroom.getName(),
+							classroom.getSeats(),
+							classroom.isProjector(),
+							classroom.isSpeakers());
+	}
+	
+	ClassroomDto classroomToClassroomDto (Classroom classroom) {
+		return new ClassroomDto(classroom.getIdClassroom(),
+							classroom.getName(),
+							classroom.getSeats(),
+							classroom.isProjector(),
+							classroom.isSpeakers());
+	}
+
+}
