2e3eb13ea04850c635c08121542149c341b20578
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-02-18 18:29:32 +0100

    refactor test package structure to match main package

--- src/test/java/dev/jcasaslopez/classroom/service/ClassroomServiceTest.java
+++ src/test/java/dev/jcasaslopez/classroom/service/ClassroomServiceTest.java
@@ -1,10 +1,10 @@
-package init;
+package dev.jcasaslopez.classroom.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ClassroomServiceApplicationTests {
+class ClassroomServiceTest {
 
 	@Test
 	void contextLoads() {
