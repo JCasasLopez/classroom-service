886aeccc56b4078438951aec121ff71dee752ad2
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-02-18 11:03:08 +0100

    Add Logback dependencies

--- src/main/java/dev/jcasaslopez/classroom/service/ClassroomServiceImpl.java
+++ src/main/java/dev/jcasaslopez/classroom/service/ClassroomServiceImpl.java
@@ -48,24 +48,25 @@ public class ClassroomServiceImpl implements ClassroomService {
 
 	@Override
 	public boolean isUserAdmin(String[] roles) {
-		/* A filter captures the token JWT used for authentication from the HTTP request header,
-		   extracts its claims and sends the roles in the form of an array as a parameter when 
-		   calling this method.
 		
-		   By default, a new user is assigned the role "user" when first created; if later 
-		   the user is upgraded to "admin", both roles are kept, hence if the array's length is 
-		   higher than 1, we know for certain that the user is an "admin".
-		   
-		   ---------------------------------------------------------------------------------------
-		
-		   Un filtro captura el token JWT usado para la autenticación de los usuarios a partir del 
+		/* Un filtro captura el token JWT usado para la autenticación de los usuarios a partir del 
 		   encabezado de la petición HTTP, extrae sus "claims" y al llamar a este método, manda el 
 		   array con los roles como parámetro.
-		   
+
 		   Por defecto, cuando se crea un nuevo usuario, se le asigna automáticamente el rol "user";
 		   si después se promociona a ese usuario a "admin", ambos roles se mantienen en su atributo
 		   "roles"; por tanto, si dicho array tiene un tamaño superior a 1, sabemos con certeza que
-		   ese usuario posee el estatus de "admin". */
+		   ese usuario posee el estatus de "admin". 
+
+		   ---------------------------------------------------------------------------------------
+
+		   A filter captures the token JWT used for authentication from the HTTP request header,
+		   extracts its claims and sends the roles in the form of an array as a parameter when 
+		   calling this method.
+
+		   By default, a new user is assigned the role "user" when first created; if later 
+		   the user is upgraded to "admin", both roles are kept, hence if the array's length is 
+		   higher than 1, we know for certain that the user is an "admin". */
 		
 		if(roles.length > 1) {
 			return true;
