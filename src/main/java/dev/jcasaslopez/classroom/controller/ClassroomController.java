89adc41f9fe8186baeea8c56a2301517c9a94264
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-02-19 22:40:15 +0100

    add deleteClassroom() and refactor createClassroom() to change HTTP status in ClassroomController

--- src/main/java/dev/jcasaslopez/classroom/controller/ClassroomController.java
+++ src/main/java/dev/jcasaslopez/classroom/controller/ClassroomController.java
@@ -5,8 +5,10 @@ import java.time.LocalDateTime;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.jcasaslopez.classroom.dto.ClassroomDto;
@@ -28,7 +30,16 @@ public class ClassroomController {
 	public ResponseEntity<StandardResponse> createClassroom(@Valid @RequestBody ClassroomDto classroom){
 		classroomService.createClassroom(classroom);
 		StandardResponse response = new StandardResponse (LocalDateTime.now(), 
-				"Classroom created successfully", null, HttpStatus.OK);
+				"Classroom created successfully", null, HttpStatus.CREATED);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+	
+	@DeleteMapping(value="/deleteClassroom")
+	public ResponseEntity<StandardResponse> deleteClassroom(@RequestParam int idClassroom){
+		classroomService.deleteClassroom(idClassroom);
+		StandardResponse response = new StandardResponse (LocalDateTime.now(), 
+				"Classroom deleted successfully", null, HttpStatus.OK);
 		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
+	
 }
