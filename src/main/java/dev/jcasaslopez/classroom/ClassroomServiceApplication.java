f4f7d67f241093ec559c443fb8b6f776496c7e46
Jorge Casas <j.casas.lopez.26@gmail.com>
2026-03-24 19:20:08 +0100

    Feat: add CommandLineRunner to publish updated list of classrooms every time the microservice is started up

--- src/main/java/dev/jcasaslopez/classroom/ClassroomServiceApplication.java
+++ src/main/java/dev/jcasaslopez/classroom/ClassroomServiceApplication.java
@@ -1,12 +1,18 @@
 package dev.jcasaslopez.classroom;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestClient;
 
+import dev.jcasaslopez.classroom.producer.ClassroomEventProducer;
+
 @SpringBootApplication
 public class ClassroomServiceApplication {
+	
+	@Autowired ClassroomEventProducer classroomEventProducer;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClassroomServiceApplication.class, args);
@@ -16,5 +22,14 @@ public class ClassroomServiceApplication {
     RestClient getClient() {
 		return RestClient.create();
 	}
+    
+    // The CommandLineRunner interface calls classroomEventProducer.publishClassrooms() to publish the updated list of
+    // classrooms, read straight from the database.  
+    @Bean
+    CommandLineRunner init() {
+        return args -> {
+        	classroomEventProducer.publishClassrooms();
+        };
+    }
 
 }
