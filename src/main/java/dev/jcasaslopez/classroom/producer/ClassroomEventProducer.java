7a6dee3e5bf5b57f605e7e764037c1af40d77f9a
Jorge Casas <j.casas.lopez.26@gmail.com>
2026-03-26 13:10:42 +0100

    Refactor: decouple classroom publishing logic and add logs

--- src/main/java/dev/jcasaslopez/classroom/producer/ClassroomEventProducer.java
+++ src/main/java/dev/jcasaslopez/classroom/producer/ClassroomEventProducer.java
@@ -1,9 +1,12 @@
 package dev.jcasaslopez.classroom.producer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+import dev.jcasaslopez.classroom.entity.Classroom;
 import dev.jcasaslopez.classroom.event.ClassroomEvent;
 import dev.jcasaslopez.classroom.mapper.ClassroomMapper;
 import dev.jcasaslopez.classroom.service.ClassroomService;
@@ -13,6 +16,8 @@ public class ClassroomEventProducer {
 	
 	@Value("${spring.kafka.producer.topic-name}")
 	private String topicName;
+	
+	private static final Logger logger = LoggerFactory.getLogger(ClassroomEventProducer.class);
 	
 	private final KafkaTemplate<String, ClassroomEvent> kafkaTemplate;
 	private final ClassroomService classroomService;
@@ -24,15 +29,22 @@ public class ClassroomEventProducer {
 		this.classroomService = classroomService;
 		this.classroomMapper = classroomMapper;
 	}
+	
+	public void publishClassroom (Classroom classroom) {
+		ClassroomEvent classroomEvent = classroomMapper.classroomToClassroomEvent(classroom);
+		kafkaTemplate.send(topicName, String.valueOf(classroom.getIdClassroom()), classroomEvent);
+		logger.info("Publish Classroom {}", classroom.getIdClassroom());
+	}
 
-	public void publishClassrooms() {
+	// This method is triggered by CommandLineRunner upon microservice startup to ensure 
+	// the initial state is synchronized with Kafka.
+	public void publishAllClassrooms() {
 		classroomService.findAll().forEach(
 				classroom -> {
-					ClassroomEvent classroomEvent = classroomMapper.classroomDtoToClassroomEvent(classroom);
-					kafkaTemplate.send(topicName, String.valueOf(classroom.getIdClassroom()), classroomEvent);
+					publishClassroom(classroomMapper.classroomDtoToClassroom(classroom));
 				}
 				);	
-		
+
 	}
 
 }
