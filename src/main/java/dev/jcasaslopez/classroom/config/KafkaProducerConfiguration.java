6da5f19f0926a6ec1f95700146203d1630481d9a
Jorge Casas <j.casas.lopez.26@gmail.com>
2026-03-26 13:16:13 +0100

    Refactor: remove hardcoded topic configuration parameters and add logs

--- src/main/java/dev/jcasaslopez/classroom/config/KafkaProducerConfiguration.java
+++ src/main/java/dev/jcasaslopez/classroom/config/KafkaProducerConfiguration.java
@@ -2,6 +2,8 @@ package dev.jcasaslopez.classroom.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.config.TopicConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,11 +15,17 @@ public class KafkaProducerConfiguration {
 	@Value("${spring.kafka.producer.topic-name}")
 	private String topicName;
 	
+	private static final Logger logger = LoggerFactory.getLogger(KafkaProducerConfiguration.class);
+	private final int numberPartitions = 1;
+	private final int numberReplicas = 3;
+
+	
 	@Bean
-    public NewTopic classroomsTopic() {
+    NewTopic classroomsTopic() {
+		logger.info("Configuring Kafka Topic: {} with {} partitions and {} replicas", topicName, numberPartitions, numberReplicas);
         return TopicBuilder.name(topicName)
-                .partitions(1)
-                .replicas(3) 
+                .partitions(numberPartitions)
+                .replicas(numberReplicas) 
                 // Use log compaction: keep only the latest snapshot per classroom ID; intermediate state changes are not required
                 .config(TopicConfig.CLEANUP_POLICY_CONFIG, TopicConfig.CLEANUP_POLICY_COMPACT)
                 .config(TopicConfig.MIN_IN_SYNC_REPLICAS_CONFIG, "2")
