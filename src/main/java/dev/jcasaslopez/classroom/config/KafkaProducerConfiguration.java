43a368bb82fd539c3a8815f222d119ca80b62d23
Jorge Casas <j.casas.lopez.26@gmail.com>
2026-03-31 18:14:48 +0200

    Refactor (Kafka config): adjust Kafka configuration

--- src/main/java/dev/jcasaslopez/classroom/config/KafkaProducerConfiguration.java
+++ src/main/java/dev/jcasaslopez/classroom/config/KafkaProducerConfiguration.java
@@ -17,7 +17,7 @@ public class KafkaProducerConfiguration {
 	
 	private static final Logger logger = LoggerFactory.getLogger(KafkaProducerConfiguration.class);
 	private final int numberPartitions = 1;
-	private final int numberReplicas = 3;
+	private final int numberReplicas = 1;
 
 	
 	@Bean
@@ -26,9 +26,21 @@ public class KafkaProducerConfiguration {
         return TopicBuilder.name(topicName)
                 .partitions(numberPartitions)
                 .replicas(numberReplicas) 
+                
                 // Use log compaction: keep only the latest snapshot per classroom ID; intermediate state changes are not required
                 .config(TopicConfig.CLEANUP_POLICY_CONFIG, TopicConfig.CLEANUP_POLICY_COMPACT)
-                .config(TopicConfig.MIN_IN_SYNC_REPLICAS_CONFIG, "2")
+                
+                // Max size of a single segment before Kafka starts a new one. Remember that Kafka does not compact a segment
+                // until has been close.
+                // 50KB which is enough for roughly 256 messages (Classroom JSON as payload).
+                .config(TopicConfig.SEGMENT_BYTES_CONFIG, "51200") 
+                
+                // Percentage of "updated/old" data allowed before Kafka starts the cleaning process.
+                .config(TopicConfig.MIN_CLEANABLE_DIRTY_RATIO_CONFIG, "0.1")
+                
+                // How long Kafka keeps the deleted messages so consumers can see them.
+                .config(TopicConfig.DELETE_RETENTION_MS_CONFIG, "86400000")
+                .config(TopicConfig.MIN_IN_SYNC_REPLICAS_CONFIG, "1")
                 .build();
     }
 
