02928aeba2cf1aa0e8dc44f824c95ae7731a7971
Jorge Casas <j.casas.lopez.26@gmail.com>
2026-04-07 11:55:23 +0200

    Fix(service): refactor constructor and cleanup logs

--- src/main/java/dev/jcasaslopez/classroom/service/AuthenticationServiceImpl.java
+++ src/main/java/dev/jcasaslopez/classroom/service/AuthenticationServiceImpl.java
@@ -8,6 +8,7 @@ import javax.crypto.SecretKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import dev.jcasaslopez.classroom.enums.RoleName;
 import dev.jcasaslopez.classroom.enums.TokenType;
@@ -18,21 +19,19 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.security.Keys;
 
+@Service
 public class AuthenticationServiceImpl implements AuthenticationService {
-
 
 	private static final Logger logger = LoggerFactory.getLogger(AuthenticationServiceImpl.class);
 	private final SecretKey key;
-	@Value("${jwt.secretKey}") String base64SecretKey;
 
-	public AuthenticationServiceImpl(SecretKey key, String base64SecretKey) {
-		this.key = key;
-		
+	public AuthenticationServiceImpl(@Value("${jwt.secretKey}") String base64SecretKey) {		
 		// Convert the Base64 encoded string back into a raw byte array.
 		byte[] keyBytes = Base64.getDecoder().decode(base64SecretKey);
 		
 		// Create a secure HMAC signing key from the bytes and validate its length.
 		key = Keys.hmacShaKeyFor(keyBytes);
+		
 		logger.info("Secret key read from application.properties, decoded and converted into SecretKey format");
 	}
 
@@ -58,7 +57,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
 			throw new FailedAuthenticatedException("Token is missing or does not start with 'Bearer '");
 		}
-		logger.debug("Token extracted");
 		return authHeader.substring(7);
 	}
 
@@ -75,7 +73,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 					// This is where all verifications happen.
 					.parseSignedClaims(token)
 					.getPayload();
-			logger.debug("Token payload extracted");
 			return claims;
 		} catch (ExpiredJwtException | MalformedJwtException | io.jsonwebtoken.security.SecurityException ex) {
 			throw new FailedAuthenticatedException("Expired or malformed token");
@@ -83,16 +80,14 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 	}
 
 	private void purposeIsValid(String purpose) {
-		if(!TokenType.VERIFICATION.name().equals(purpose)) {
-			throw new FailedAuthenticatedException("Token is not a verification token");
+		if(!TokenType.ACCESS.prefix().equals(purpose)) {
+			throw new FailedAuthenticatedException("Token is not a access token");
 		}
-		logger.debug("Verification token validated");
 	}
 
 	private void rolesAreValid(List<String> roles) {
 		for(String rol:roles) {
 			if(rol.equals(RoleName.ROLE_ADMIN.name()) || rol.equals(RoleName.ROLE_SUPERADMIN.name())) {
-				logger.debug("Admin credentials validated");
 				return;
 			}
 		}
