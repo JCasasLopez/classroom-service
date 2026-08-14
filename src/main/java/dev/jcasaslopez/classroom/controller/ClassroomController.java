56e47bdd6ea7cfbc9498e1a56afcc41c06b20298
Jorge Casas <j.casas.lopez.26@gmail.com>
2026-08-14 18:29:06 +0200

    feat: add 'Get Classrooms List' endpoint

--- src/main/java/dev/jcasaslopez/classroom/controller/ClassroomController.java
+++ src/main/java/dev/jcasaslopez/classroom/controller/ClassroomController.java
@@ -1,10 +1,13 @@
 package dev.jcasaslopez.classroom.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.jcasaslopez.classroom.dto.ClassroomRequestDto;
+import dev.jcasaslopez.classroom.dto.ClassroomResponseDto;
 import dev.jcasaslopez.classroom.service.ClassroomService;
 import dev.jcasaslopez.classroom.shared.utility.StandardResponse;
 import dev.jcasaslopez.classroom.util.ClassroomEndpoints;
@@ -45,6 +49,13 @@ public class ClassroomController {
     public ResponseEntity<StandardResponse<Void>> deleteClassroom(@PathVariable int id) {
         classroomService.deleteClassroom(id);
         StandardResponse<Void> response = new StandardResponse<>("Classroom deleted successfully", null, HttpStatus.OK);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+    
+    @GetMapping(ClassroomEndpoints.CLASSROOMS)
+    public ResponseEntity<StandardResponse<List<ClassroomResponseDto>>> getClassroomsList() {
+        StandardResponse<List<ClassroomResponseDto>> response = new StandardResponse<>(
+        					"Classrooms list retrieved successfully", classroomService.findAll(), HttpStatus.OK);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
