d3e58a64c2f7090b9f2200371a32d72fbc53fddc
Jorge Casas <j.casas.lopez.26@gmail.com>
2026-08-31 13:09:24 +0200

    test: create test profile and remove Hibernate DDL Auto property from test

--- src/test/java/dev/jcasaslopez/classroom/base/BaseRepositoryTest.java
+++ src/test/java/dev/jcasaslopez/classroom/base/BaseRepositoryTest.java
@@ -4,13 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 
 import dev.jcasaslopez.classroom.repository.ClassroomRepository;
 
 @DataJpaTest
-@TestPropertySource(properties = "spring.jpa.hibernate.ddl-auto=create-drop")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public abstract class BaseRepositoryTest {
 
